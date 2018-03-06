eb127f4c1f2e34134f913e0244941aa52cde8ba7
abelko <esung1129@gmail.com>
2018-03-06 10:55:05 +0900

    spaceNavigator완성하기

---
+++ nasa-apod/src/components/Viewer/Viewer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Viewer.scss';
+import classNames from 'classnames/bind';
+const cx = classNames.bind(styles);
+
+const Viewer = ({mediaType, url, loading}) => {
+  return (
+    <div className={cx('viewer')}>
+
+    </div>
+  );
+};
+
+export default Viewer;
