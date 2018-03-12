d9eed29baef2a9903c1a172e3d0985bbc5f5e572
abelko <esung1129@gmail.com>
2018-03-12 22:18:26 +0900

    Provider 컴포넌트를 사용하여 리액트 앱에 store연동하기

--- redux-practice/counter1/src/index.js
+++ redux-practice/counter1/src/index.js
@@ -7,11 +7,14 @@ import registerServiceWorker from './registerServiceWorker';
 // Redux 관련 불러오기
 import { createStore } from 'redux';
 import reducers from './reducers';
+import { Provider } from 'react-redux';
 
 // 스토어 생성
 const store = createStore(reducers);
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('root'));
 registerServiceWorker();
