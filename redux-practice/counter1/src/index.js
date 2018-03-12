7f1ba81588977de4c8d00d668f2139b7f97e978e
abelko <esung1129@gmail.com>
2018-03-12 22:16:27 +0900

    Store만들기

--- redux-practice/counter1/src/index.js
+++ redux-practice/counter1/src/index.js
@@ -4,5 +4,14 @@ import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Redux 관련 불러오기
+import { createStore } from 'redux';
+import reducers from './reducers';
+
+// 스토어 생성
+const store = createStore(reducers);
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root'));
 registerServiceWorker();
