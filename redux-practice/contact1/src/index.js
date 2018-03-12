db1953e1559c6b82fee3da9416c534446ff3b35d
abelko <esung1129@gmail.com>
2018-03-12 15:08:56 +0900

    모듈 만들기

--- redux-practice/contact1/src/index.js
+++ redux-practice/contact1/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+
+import { createStore } from 'redux';
+import reducers from './modules';
+import { Provider } from 'react-redux';
+import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// 스토어 생성
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
