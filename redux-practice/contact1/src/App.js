031ac5617499152c06b43c88128acf92210545f7
abelko <esung1129@gmail.com>
2018-03-12 16:10:08 +0900

    ViewSelectorContainer 컴포넌트 만들기

--- redux-practice/contact1/src/App.js
+++ redux-practice/contact1/src/App.js
@@ -3,6 +3,8 @@ import Header from './components/Header';
 import Container from './components/Container';
 import { connect } from 'react-redux';
 
+import ViewSelectorContainer from './containers/ViewSelectorContainer';
+
 class App extends Component {
     render() {
         //레퍼런스 준비
@@ -11,7 +13,7 @@ class App extends Component {
         return (
             <div>
                 <Header/>
-                {/* ViewSelectorContainer */}
+                <ViewSelectorContainer/>
 
                 {/* view 값에 따라 다른 컨테이너를 보여준다 */}
                 <Container visible={view==='favorite'}>
