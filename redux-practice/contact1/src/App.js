90fef3c3bc8124790fc0d8fc99022dfbaa8dc2cb
abelko <esung1129@gmail.com>
2018-03-12 19:36:07 +0900

    ContactListContainer 컴포넌트 만들기

--- redux-practice/contact1/src/App.js
+++ redux-practice/contact1/src/App.js
@@ -8,6 +8,7 @@ import InputContainer from './containers/InputContainer';
 import FavoriteListContainer from './containers/FavoriteListContainer';
 import FloatingButtonContainer from './containers/FloatingButtonContainer';
 import ContactModalContainer from './containers/ContactModalContainer';
+import ContactListContainer from './containers/ContactListContainer';
 
 class App extends Component {
     render() {
@@ -25,7 +26,7 @@ class App extends Component {
                 </Container>
                 <Container visible={view==='list'}>
                     <InputContainer />
-                    {/* ContactListContainer */}
+                    <ContactListContainer />
                 </Container>
 
                 <ContactModalContainer />
