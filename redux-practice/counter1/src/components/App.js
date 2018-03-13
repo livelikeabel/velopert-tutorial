2fd0dc5fd0386f4811d5fd9ae0b4bbc1d343be85
abelko <esung1129@gmail.com>
2018-03-13 10:51:40 +0900

    CounterContainer 컴포넌트 만들기

--- redux-practice/counter1/src/components/App.js
+++ redux-practice/counter1/src/components/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import Counter from './Counter';
+import CounterContainer from '../containers/CounterContainer';
 
 class App extends Component {
     render() {
         return (
             <div>
-                Counter
-                <Counter />
+                <CounterContainer/>
             </div>
         );
     }
