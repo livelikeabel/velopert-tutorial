4f7b9da441e4a130fbcf0d5708fbdb6c01d83ae9
abelko <esung1129@gmail.com>
2018-03-13 12:48:40 +0900

    컨테이너 컴포넌트 만들기

--- redux-practice/counter1/src/components/App.js
+++ redux-practice/counter1/src/components/App.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
-import Counter from './Counter';
-import CounterContainer from '../containers/CounterContainer';
 import Buttons from './Buttons';
+import CounterListContainer from '../containers/CounterListContainer';
+
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+import { getRandomColor } from '../utils';
 
 class App extends Component {
     render() {
+        const { onCreate, onRemove } = this.props;
         return (
-            <div>
-                <Buttons />
-                <CounterContainer/>
+            <div className="App">
+                <Buttons
+                    onCreate={onCreate}
+                    onRemove={onRemove}
+                />
+              <CounterListContainer/>
             </div>
         );
     }
 }
 
-export default App;
+//액션함수 준비
+const mapToDispatch = (dispatch) => ({
+    onCreate: () => dispatch(actions.create(getRandomColor())),
+    onRemove: (index) => dispatch(actions.remove(index))
+});
+
+// 리덕스에 연결을 시키고 내보낸다.
+export default connect(null, mapToDispatch)(App);
