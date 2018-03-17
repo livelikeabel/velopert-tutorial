8bd1cf5ef36f8181411d2f456cbb1caaa252b911
abelko <esung1129@gmail.com>
2018-03-17 22:10:23 +0900

    styled-components

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Button from './components/Button';
+import StyledButton from './components/StyledButton';
 
 class App extends Component {
   render(){
     return(
       <div>
-        <Button>버튼</Button>
+         <Button>버튼</Button>
+         <StyledButton big>스타일 버튼</StyledButton>
       </div>
     )
   }
