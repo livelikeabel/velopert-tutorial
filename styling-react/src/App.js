845747544c25b34e9134601d8d43ac1558beaf4a
abelko <esung1129@gmail.com>
2018-03-17 21:42:30 +0900

    Sass

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react';
-import classNames from 'classnames/bind';
-import styles from './App.css';
-const cx = classNames.bind(styles);
-
-console.log(styles);
+import Button from './components/Button';
 
 class App extends Component {
-  render() {
-    const isBlue = true;
-
-    return (
-      <div className={cx('box', {
-          blue: isBlue
-      })}>
-
+  render(){
+    return(
+      <div>
+        <Button>버튼</Button>
       </div>
-    );
+    )
   }
 }
 
---
+++ styling-react/src/App2.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import classNames from 'classnames/bind';
+import styles from './App.scss';
+const cx = classNames.bind(styles);
+
+console.log(styles);
+
+class App extends Component {
+  render() {
+    const isBlue = true;
+
+    return (
+      <div className={cx('box', {
+          blue: isBlue
+      })}>
+        <div className={cx('box-inside')}/>
+      </div>
+    );
+  }
+}
+
+export default App;
