32d4ff7ca8093e64cea282b167cd34f28156d543
abelko <esung1129@gmail.com>
2018-04-02 23:50:11 +0900

    6. ContactModal 완성하기

--- contact1/src/App.js
+++ contact1/src/App.js
@@ -5,28 +5,29 @@ import ViewSelector from './components/ViewSelector';
 import FloatingButton from './components/FloatingButton';
 import ContactModal from './components/ContactModal';
 import Dimmed from './components/Dimmed';
+import shortid from 'shortid';
 import oc from 'open-color';
 
 function generateRandomColor() {
-    const colors = [
-      'gray',
-      'red',
-      'pink',
-      'grape',
-      'violet',
-      'indigo',
-      'blue',
-      'cyan',
-      'teal',
-      'green',
-      'lime',
-      'yellow',
-      'orange'
-    ];
+  const colors = [
+    'gray',
+    'red',
+    'pink',
+    'grape',
+    'violet',
+    'indigo',
+    'blue',
+    'cyan',
+    'teal',
+    'green',
+    'lime',
+    'yellow',
+    'orange'
+  ];
 
-    const random = Math.floor(Math.random() * 13);
+  const random = Math.floor(Math.random() * 13);
 
-    return oc[colors[random][6]];
+  return oc[colors[random][6]];
 }
 
 class App extends Component {
@@ -36,77 +37,112 @@ class App extends Component {
     modal: {
       visible: false,
       mode: null // create 혹은 modify
-    }
+    },
+    contacts: []
   }
 
-  handleSelectView = (view) => this.setState({view})
+  handleSelectView = (view) => this.setState({ view })
 
   modalHandler = {
     show: (mode, payload) => {
-        this.setState({
-          modal: {
-            mode,
-            visible: true,
-            ...payload // payload 안의 깂을 풀어서 여기에 넣음
-          }
-        })
+      this.setState({
+        modal: {
+          mode,
+          visible: true,
+          ...payload // payload 안의 깂을 풀어서 여기에 넣음
+        }
+      })
     },
     hide: () => {
-        this.setState({
-          modal: {
-              ...this.state.modal, // 기존 값들을 복사해서 안에 넣음
-              visible: false
-          }
-        })
+      this.setState({
+        modal: {
+          ...this.state.modal, // 기존 값들을 복사해서 안에 넣음
+          visible: false
+        }
+      })
     },
-
-    change: null,
+    change: ({ name, value }) => {
+      this.setState({
+        modal: {
+          ...this.state.modal,
+          [name]: value
+        }
+      })
+    },
     action: {
-        create: null,
-        modify: null,
-        remove: null
+      create: () => {
+        // 고유 ID 생성
+        const id = shortid.generate();
+
+        // 레퍼런스 생성
+        const { contacts, modal: { name, phone, color } } = this.state;
+
+        // 데이터 생성
+        const contact = {
+          id,
+          name,
+          phone,
+          color,
+          favorite: false // 즐겨찾기의 기본값은 false
+        };
+
+        this.setState({
+          // 기존 배열에있던것들을 집어넣고, contact 를 뒤에 추가한 새 배열로 설정
+          contacts: [...contacts, contact]
+        });
+
+        // 모달 닫기
+        this.modalHandler.hide();
+      },
+      modify: null,
+      remove: null
     }
   }
 
   handleFloatingButtonClick = () => {
     const { view } = this.state;
-    if(view !== 'list')
-        this.setState({view: 'list'});
+    if (view !== 'list')
+      this.setState({ view: 'list' });
 
     this.modalHandler.show(
-        'create',
-        {
-            name: '',
-            phone: '',
-            color: generateRandomColor()
-        }
+      'create',
+      {
+        name: '',
+        phone: '',
+        color: generateRandomColor()
+      }
     );
   }
 
   render() {
 
-    const { 
+    const {
       handleSelectView,
       handleFloatingButtonClick,
       modalHandler
-     } = this;
+    } = this;
 
-    const { 
+    const {
       view,
       modal
-     } = this.state;
+    } = this.state;
 
     return (
       <div>
-          <Header/>
-          <ViewSelector onSelect={handleSelectView} selected={view}/>
+        <Header />
+        <ViewSelector onSelect={handleSelectView} selected={view} />
 
-          <Container visible={view==='favorite'}>즐겨찾기</Container>
-          <Container visible={view==='favorite'}>리스트</Container>
+        <Container visible={view === 'favorite'}>즐겨찾기</Container>
+        <Container visible={view === 'favorite'}>리스트</Container>
 
-          <ContactModal {...modal} onHide={modalHandler.hide}/>
-          <Dimmed visible={modal.visible}/>
-          <FloatingButton onClick={handleFloatingButtonClick}/>
+        <ContactModal
+          {...modal}
+          onHide={modalHandler.hide}
+          onChange={modalHandler.change}
+          onAction={modalHandler.action[modal.mode]}
+        />
+        <Dimmed visible={modal.visible} />
+        <FloatingButton onClick={handleFloatingButtonClick} />
       </div>
     );
   }
