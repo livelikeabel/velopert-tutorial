d65eadc7fd1f46c4eb1c01972926d8288b52ba16
abelko <esung1129@gmail.com>
2018-04-02 21:46:48 +0900

    기본 컴포넌트 만들기

---
+++ contact1/src/components/Container.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+import oc from 'open-color';
+import { media } from '../lib/style-utils';
+import PropTypes from 'prop-types';
+
+const Wrapper = styled.div`
+    width: 700px;
+    margin: 0 auto;
+    padding: 1rem;
+    background: black;
+
+    ${media.mobile`
+        width: 100%;
+    `}
+`;
+
+// visible이 false이면 null반환
+const Container = ({visible, children}) => visible ? (
+    <Wrapper>
+        {children}
+    </Wrapper>
+) : null;
+
+Container.propTypes = {
+    visible: PropTypes.bool
+};
+
+export default Container;
