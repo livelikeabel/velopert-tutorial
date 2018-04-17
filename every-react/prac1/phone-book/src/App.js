f76ba3134efffef444fcf0f4f91d05d31819495d
abelko <esung1129@gmail.com>
2018-04-17 10:27:16 +0900

    배열 다루기(1) 생성과 렌더링

--- every-react/prac1/phone-book/src/App.js
+++ every-react/prac1/phone-book/src/App.js
@@ -2,13 +2,33 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import PhoneForm from "./components/PhoneForm";
+import PhoneInfoList from "./components/PhoneInfoList";
 
 class App extends Component {
-  handleCreate = data => {
-    console.log(data);
+  id = 2;
+  state = {
+    information: [
+      {
+        id: 0,
+        name: "abelko",
+        phone: "010-4943-4355"
+      },
+      {
+        id: 1,
+        name: "kkyou",
+        phone: "010-4433-4125"
+      }
+    ]
   };
-
+  handleCreate = data => {
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({ id: this.id++, ...data })
+    });
+  };
   render() {
+    const { information } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
@@ -16,6 +36,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <PhoneForm onCreate={this.handleCreate} />
+        <PhoneInfoList data={this.state.information} />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
